3da91f7b6dbf3b010a01e466c03b3efa70f10654
Akshansh Chandra <akshansh@relinns.com>
2025-06-14 23:21:46 +0530

    Flask

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 from datetime import datetime, timedelta
 import os
 
-app = Flask(Medoplus)
+app = Flask(__name__)
 app.secret_key = "your_secret_key"
 
 # MongoDB setup
